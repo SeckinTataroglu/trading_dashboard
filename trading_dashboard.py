ab0ce2e1a33efa097536d469ec6f242c826f1996
Seckin Tataroglu <seckintataroglu@gmail.com>
2023-08-27 23:19:46 +0300

    plotly integration

--- trading_dashboard.py
+++ trading_dashboard.py
@@ -1,7 +1,9 @@
 import streamlit as st
 import yfinance as yf
 import pandas as pd
-import matplotlib.pyplot as plt
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime, timedelta
 from scipy.stats import zscore
 
@@ -10,16 +12,18 @@ st.set_page_config(layout="wide")
 # Function to fetch a list of stock symbols from a file or API
 def get_stock_symbols():
 
-    turkey_tickers = [
+    turkey_tickers = list(set([
         "---Turkey---",
         'AKBNK.IS', 'GARAN.IS', 'ISCTR.IS', 'KCHOL.IS', 'ASELS.IS',  # Turkey's banks and companies
         'TTKOM.IS', 'TCELL.IS', 'VESTL.IS', 'PETKM.IS', 'TKFEN.IS',  # Telecom, energy, and industrial companies
         'SAHOL.IS', 'ULKER.IS', 'KOZAL.IS', 'TATGD.IS', 'AYGAZ.IS',  # Food, mining, and retail companies in Turkey
+        "EGSER.IS", "AFYON.IS", "BUCIM.IS", "EREGL.IS", "BSOKE.IS",
+        "ADEL.IS",
         # Add more Turkish tickers as needed
-    ]
+    ]))
     turkey_tickers.sort()
 
-    uk_tickers = [
+    uk_tickers = list(set([
          "-----UK-----",
         'AAPL', 'MSFT', 'GOOGL', 'AMZN', 'TSLA',  # Example of non-UK tickers from previous list
         'HSBA.L', 'LLOY.L', 'BARC.L', 'RBS.L', 'RDSA.L',  # UK banks and energy companies
@@ -27,7 +31,7 @@ def get_stock_symbols():
         'TSCO.L', 'MKS.L', 'RR.L', 'IAG.L', 'CCL.L',  # Retail, airline, and consumer goods companies
         'BP.L', 'RDSB.L', 'STAN.L', 'NG.L', 'GLEN.L',  # UK energy and mining companies
         # Add more UK tickers as needed
-    ]
+    ]))
     uk_tickers.sort()
 
 
@@ -75,7 +79,7 @@ def main():
         interval = st.selectbox('Select Interval:', interval_list)
 
         today = datetime.today()
-        max_days = 10000
+        max_days = 1825
         if interval in ["1m", "5m", "15m", "30m", "60m", "90m"]:
             max_days = 60-1
         if interval in ["1h"]:
@@ -93,8 +97,8 @@ def main():
         
         if df is not None:
             
-            with st.sidebar:
-                st.write(df)
+            # with st.sidebar:
+            #     st.write(df)
             
             with col1:
                 st.title('Close Prices')
@@ -103,22 +107,28 @@ def main():
                 st.title('Stationary Data')
                 st.line_chart(df['Return'])
 
+                st.title('Z-Score Data')
+                st.line_chart(df['Z-Score'])
+
             with col2:
                 ### Stationary DF
                 st.title(' ')
                 st.write(df)
 
-                ### Return Histogram
-                st.title('Return Hist')
-                fig, ax = plt.subplots()
-                ax.hist(df['Return'].dropna(), bins=20, edgecolor="black", facecolor='#9ac8fd', alpha=1)
-                st.pyplot(fig)
+                ## Return Histogram
+                # st.title('Return Hist')
+                fig = px.histogram(df['Return'].dropna())
+                fig.update_layout(xaxis_title='Return %', yaxis_title='Frequency', showlegend=False)
+                st.plotly_chart(fig)
+
 
                 ### Z-Score Histogram
-                st.title('Z-Score Hist')
-                fig, ax = plt.subplots()
-                ax.hist(df['Z-Score'].dropna(), bins=20, edgecolor="black", facecolor='#9ac8fd', alpha=1)
-                st.pyplot(fig)
+                # st.title('Z-Score Hist')
+                bin_edges = [-3, -2.5, -2, -1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2, 2.5, 3]
+                hist_data, _ = np.histogram(df['Z-Score'].dropna(), bins=bin_edges)
+                fig = go.Figure(data=[go.Bar(x=bin_edges[:-1], y=hist_data, marker_color='#9ac8fd')])
+                fig.update_layout(xaxis_title='Z-Score', yaxis_title='Frequency', showlegend=False)
+                st.plotly_chart(fig)
 
             
 
